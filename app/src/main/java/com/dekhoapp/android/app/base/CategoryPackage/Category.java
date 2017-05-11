e7719e493e5940f4fcfac6b34fe41211e2ebc540
SoumyaAgarwal <soumya.agarwal@gmail.com>
2017-05-11 14:39:58 +0530

    First Commit

--- app/src/main/java/com/dekhoapp/android/app/base/CategoryPackage/Category.java
+++ app/src/main/java/com/dekhoapp/android/app/base/CategoryPackage/Category.java
@@ -1,11 +1,12 @@
 package com.dekhoapp.android.app.base.CategoryPackage;
 
+import android.animation.ObjectAnimator;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -13,7 +14,11 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.dekhoapp.android.app.base.ListOfSongsPackage.List_of_Songs;
@@ -23,34 +28,60 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.R.attr.animation;
 
 public class Category extends AppCompatActivity {
 
     private List<CategoryClass> categoryList = new ArrayList<>();
     private List<CategoryClass> m = new ArrayList<>();
     public List<CategoryClass> filtered = new ArrayList<>();
-    public RecyclerView recyclerView;
+    public EmptyRecyclerView recyclerView;
     private CategoryAdapter mAdapter;
     private EditText edit;
-    GridLayoutManager mLayoutManager;
+    //GridLayoutManager mLayoutManager;
     DatabaseReference mDatabase,db;
+    private static final int TOTAL_ITEM_EACH_LOAD = 7;
+    private int currentPage = 0;
+    LinearLayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
 
-        recyclerView = (RecyclerView) findViewById(R.id.rv_category);
+        recyclerView = (EmptyRecyclerView) findViewById(R.id.rv_category);
         edit=(EditText)findViewById(R.id.edit);
+
+        LinearLayout emptyView = (LinearLayout) findViewById(R.id.todo_list_empty_view);
+
+        ProgressBar progressBar = (ProgressBar)findViewById(R.id.progress);
+        ObjectAnimator animation = ObjectAnimator.ofInt (progressBar, "progress", 0, 500); // see this max value coming back here, we animale towards that value
+        animation.setDuration(2000);
+        animation.setInterpolator (new DecelerateInterpolator());
+        animation.setRepeatCount(Animation.INFINITE);
+        animation.start ();
+        recyclerView.setEmptyView(emptyView);
+
         mDatabase = FirebaseDatabase.getInstance().getReference();
         //set adapter
         addingrows(categoryList);
         m=categoryList;
 
-        //listener for rows in recyclerview
+        recyclerView.setOnScrollListener(new EndlessRecyclerOnScrollListener(mLayoutManager) {
+            @Override
+            public void onLoadMore(int current_page) { // when we have reached end of RecyclerView this event fired
+                loadMoreData();
+            }
+        });
+        loadData();
+
+        //preparecategoryData();
+
         recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), recyclerView, new ClickListener() {
             @Override
             public void onClick(View view, int position) {
@@ -66,9 +97,6 @@ public class Category extends AppCompatActivity {
             }
         }));
 
-        //adding rows to recyclerview
-//        new ChildAddAsync().execute();
-            preparecategoryData();
         //search functionality
         edit.addTextChangedListener(new TextWatcher() {
             @Override
@@ -129,11 +157,64 @@ public class Category extends AppCompatActivity {
     private void addingrows(final List<CategoryClass> k)
     {
         mAdapter = new CategoryAdapter(getApplicationContext(),k);
-        //RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
-        mLayoutManager=new GridLayoutManager(Category.this,2);
+        mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        //mLayoutManager=new GridLayoutManager(Category.this,2);
         recyclerView.setLayoutManager(mLayoutManager);
         //recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(mAdapter);
+    }
+    private void loadData() {
+        // example
+        // at first load : currentPage = 0 -> we startAt(0 * 10 = 0)
+        // at second load (first loadmore) : currentPage = 1 -> we startAt(1 * 10 = 10)
+        db = mDatabase.child("Soumya").child("Music").getRef();
+
+        db.limitToFirst(TOTAL_ITEM_EACH_LOAD)
+                .startAt(currentPage*TOTAL_ITEM_EACH_LOAD)
+                .orderByChild("number").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s)
+            {
+                if(!dataSnapshot.hasChildren())
+                {
+                    Toast.makeText(Category.this, "No more Categories", Toast.LENGTH_SHORT).show();
+                    currentPage--;
+                }
+
+                String category = dataSnapshot.getKey();
+
+                    CategoryClass ct = new CategoryClass(R.drawable.song,category);
+                    categoryList.add(ct);
+                    mAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    private void loadMoreData(){
+        currentPage++;
+        loadData();
     }
 
     private void preparecategoryData()
@@ -173,36 +254,4 @@ public class Category extends AppCompatActivity {
         });
     }
 
-    class ChildAddAsync extends AsyncTask<String, String, String> {
-
-        ProgressDialog pd;
-
-        @Override
-        protected void onPreExecute() {
-
-            pd = new ProgressDialog(getApplicationContext());
-            pd.setMessage("Loading...");
-            pd.setIndeterminate(false);
-            pd.setCancelable(true);
-            pd.show();
-        }
-
-        @Override
-        protected String doInBackground(final String... params) {
-            try {
-                preparecategoryData();
-            }
-            catch (Exception e)
-            {
-                e.printStackTrace();
-                Log.d("Failure", "FAILURE,FAILURE 111111");
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String j) {
-
-        }
-    }
 }
